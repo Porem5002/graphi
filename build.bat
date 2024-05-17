64897c9b8caf7d45297f0faeb6a8189c643a54ac
Carlos Bandeira <porem5002@gmail.com>
2024-05-17 22:59:09 +0100

    Changed build.bat to allow for easily building release and debug builds

--- build.bat
+++ build.bat
@@ -1,1 +1,13 @@
-odin build src -out:graphi.exe -o:speed -vet
+@echo off
+
+if "%1%" == "clean" (
+    echo Cleaning...
+    del *.exe
+    del *.pdb
+) else if "%1%" == "release" (
+    echo Building Release Build...
+    odin build src -out:graphi.exe -o:speed -subsystem:windows -vet
+) else (
+    echo Building Debug Build...
+    odin build src -out:graphi.exe -o:none -debug -vet
+)
